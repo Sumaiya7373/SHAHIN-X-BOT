abd07576e9da6bf0e9e7df94e95733e5c9f49a41
Sumaiya7373 <ranashahin11102005@gmail.com>
2026-05-27 09:54:49 +0600

    Enhance promotion message in promote.js
    Updated the promotion message format to include the promoter's name and improved text styling.

--- grpUpCmd/promote.js
+++ grpUpCmd/promote.js
@@ -1,13 +1,33 @@
 module.exports = {
+
   event: 'promote',
+
   handle: async ({ api, event }) => {
+
     const promotedMembers = event.participants;
+
+    const promoter = event.author || event.sender || "Unknown";
+
     console.log(event);
+
     for (const member of promotedMembers) {
+
+      const user = member.split('@')[0];
+
+      const by = promoter.split('@')[0];
+
       await api.sendMessage(event.id, {
-        text: `🎉⎯͢✧🫰 𝐂ᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴꜱᥫ᭡  @${member.split('@')[0]}! ⎯͢✧𝐘ᴏᴜ ᴀʀᴇ ɴᴏᴡ ᴀɴ 𝐀ᴅᴍɪɴ!`,
-        mentions: [member]
+
+        text: `🎉 ⎯͢✧𝐂ᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴ𝐒 @${user}! ⎯͢✧𝐘ᴏᴜ ᴀʀᴇ ɴᴏᴡ ᴀɴ 𝐀ᴅᴍɪɴ!
+
+👑 ⎯͢✧𝐏ʀᴏᴍᴏᴛᴇᴅ ʙʏ: @${by}`,
+
+        mentions: [member, promoter]
+
       });
+
     }
+
   }
+
 };
